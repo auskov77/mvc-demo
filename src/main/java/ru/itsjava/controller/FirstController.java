4a1aa8959ae7c308b9f2a8926c3915a391121649
auskov77 <auskov77@yandex.ru>
2022-06-27 12:12:39 +0300

    Initial commit

---
+++ src/main/java/ru/itsjava/controller/FirstController.java
@@ -0,0 +1,13 @@
+package ru.itsjava.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/")
+    public String getForAll(){
+        return "index";
+    }
+}
---
+++ src/main/java/ru/itsjava/domain/Notebook.java
@@ -0,0 +1,12 @@
+package ru.itsjava.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class Notebook {
+    private final String firm;
+}
